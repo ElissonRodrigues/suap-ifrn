b5470784e9e40d62397630379cdf7fcb32a88f46
Elisson Rodrigues <elissonrodrigues98@gmail.com>
2023-06-21 04:08:11 +0000

    correções e melhorias

--- setup.sh
+++ setup.sh
@@ -2,7 +2,19 @@ GREEN='\033[0;32m'
 RED='\033[1;31m'
 NC='\033[0m' # Sem cor (reset)
 
-login_code() {
+criar_internetSH() {
+    echo -e "\n${GREEN}Criando arquivo 'internet.sh' para conexão manual com a internet...${NC}"
+    sleep 4
+    echo 'echo -n "Matricula do Suap: "' > ~/internet.sh
+    echo 'read username' >> ~/internet.sh
+    echo 'echo -n "Senha do Suap: "' >> ~/internet.sh
+    echo 'read -s password' >> ~/internet.sh
+    echo 'curl -k -d "escapeUser=$username&user=$username&passwd=$password&ok=Login" -X POST "https://autenticacao-sga.ifrn.local:6082/php/uid.php?vsys=1&rule=0"' >> ~/internet.sh
+    chmod +x ~/internet.sh
+    clear
+}
+
+criar_bashrc() {
     echo -e "${GREEN}Criando linhas para conexão com a internet no .bashrc${NC}"
     sleep 3
     echo ' ' >> ~/.bashrc
@@ -17,16 +29,7 @@ login_code() {
     echo '    curl -k -d "escapeUser=$username&user=$username&passwd=$password&ok=Login" -X POST "https://autenticacao-sga.ifrn.local:6082/php/uid.php?vsys=1&rule=0"' >> ~/.bashrc
     echo 'fi' >> ~/.bashrc
     
-    echo -e "\n${GREEN}Criando arquivo "internet.sh" para conexão manual com a internet...${NC}"
-    sleep 4
-    touch internet.sh
-    echo '"Matricula do Suap: "' > ~/.internet.sh
-    echo 'read username' >> ~/.internet.sh
-    echo 'echo -n "Senha do Suap: "' >> ~/.internet.sh
-    echo 'read -s password' >> ~/.internet.sh
-    echo 'curl -k -d "escapeUser=$username&user=$username&passwd=$password&ok=Login" -X POST "https://autenticacao-sga.ifrn.local:6082/php/uid.php?vsys=1&rule=0"' >> ~/.internet.sh
-    clear
-    chmod +x internet.sh
+    criar_internetSH
     echo -e "${GREEN}O script de conexão está pronto!\n\nNo próximo login, caso necessário, será solicitado o nome de usuário e senha do SUAP.${NC}"
 }
 
@@ -34,12 +37,12 @@ if [ -f ~/.bashrc ]; then
     clear
     echo -e "${RED}Arquivo .bashrc já existe.${NC}"
     sleep 3
-    login_code
+    criar_bashrc
 else
     echo -e "\n${GREEN}Criando arquivo .bashrc...${NC}"
     touch .bashrc
     echo -e "\n${GREEN}Arquivo .bashrc criado com sucesso.${NC}"
-    login_code
+    criar_bashrc
 fi
 
 #©Elisson Rodrigues - github.com/ElissonRodrigues
